86276b4d7c0f8d71dece46271b93241e7acf4a60
Senith <rsenith@gmail.com>
2016-08-09 01:21:55 +0530

    contact us added

--- Contact.php
+++ Contact.php
@@ -45,6 +45,7 @@
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
+  	
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
@@ -55,6 +56,46 @@
         <li><a href="#"><i class="fa fa-dashboard"></i> Contact US</a></li>
         
       </ol>
+    </section>
+
+    <section class="content">
+    	
+    	<form class="form-horizontal" role="form" method="post" action="index.php">
+	<div class="form-group">
+		<label for="name" class="col-sm-2 control-label">Name</label>
+		<div class="col-sm-10">
+			<input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name" value="">
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="email" class="col-sm-2 control-label">Email</label>
+		<div class="col-sm-10">
+			<input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" value="">
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="message" class="col-sm-2 control-label">Message</label>
+		<div class="col-sm-10">
+			<textarea class="form-control" rows="4" name="message"></textarea>
+		</div>
+	</div>
+	<!--<div class="form-group">
+		<label for="human" class="col-sm-2 control-label">2 + 3 = ?</label>
+		<div class="col-sm-10">
+			<input type="text" class="form-control" id="human" name="human" placeholder="Your Answer">
+		</div>
+	</div>-->
+	<div class="form-group">
+		<div class="col-sm-10 col-sm-offset-2">
+			<input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
+		</div>
+	</div>
+	<div class="form-group">
+		<div class="col-sm-10 col-sm-offset-2">
+			<! Will be used to display an alert to the user>
+		</div>
+	</div>
+</form>
     </section>
 
     
